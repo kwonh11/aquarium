f28f0a60b9af80bd33ebf3f8afecafb9493ed9ae
kwonh11 <kwonh11@gmail.com>
2020-07-09 23:49:30 +0900

    Updates

---
+++ precache-manifest.ace9e41214267a123bed3620b8dc463a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564fcc05f313c43ff2c46ed200d4c2f6",
+    "url": "/aquarium/index.html"
+  },
+  {
+    "revision": "14be3c47d18063adc5fe",
+    "url": "/aquarium/static/js/2.d7221a2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aquarium/static/js/2.d7221a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b245f3970836700cc832",
+    "url": "/aquarium/static/js/main.76ca80a7.chunk.js"
+  },
+  {
+    "revision": "653eb25e839891b06ca3",
+    "url": "/aquarium/static/js/runtime-main.d7e98283.js"
+  },
+  {
+    "revision": "f9e80f995835f3bc0350d68e47e74285",
+    "url": "/aquarium/static/media/63-aqua-planet.f9e80f99.png"
+  },
+  {
+    "revision": "a5dbdd19cf25cc332e10b9465ee6fe53",
+    "url": "/aquarium/static/media/63_gate_video.a5dbdd19.mp4"
+  },
+  {
+    "revision": "6108c5d9704e968ead1f4aa217ad44bb",
+    "url": "/aquarium/static/media/common_logo.6108c5d9.png"
+  },
+  {
+    "revision": "e9575b46a9dc62e8105e50903d9992ae",
+    "url": "/aquarium/static/media/gate_bg.e9575b46.jpg"
+  },
+  {
+    "revision": "1a2f123cf660be75e861434cc2acf8cc",
+    "url": "/aquarium/static/media/ilsan-aqua-planet.1a2f123c.png"
+  },
+  {
+    "revision": "526b55511fb4a0f9caa3c951b9119ae8",
+    "url": "/aquarium/static/media/ilsan_gate_video.526b5551.mp4"
+  },
+  {
+    "revision": "b437e5ea58c95715dfed2d19654b18df",
+    "url": "/aquarium/static/media/jeju-aqua-planet.b437e5ea.png"
+  },
+  {
+    "revision": "2adaaa26f952494024a5a41483a7f974",
+    "url": "/aquarium/static/media/jeju_gate_video.2adaaa26.mp4"
+  },
+  {
+    "revision": "1d20ff563145d9f70c119488714b26e0",
+    "url": "/aquarium/static/media/qwanggyo_gate_video.1d20ff56.mp4"
+  },
+  {
+    "revision": "0f4e70577b4c53b2dfc17bafbd731844",
+    "url": "/aquarium/static/media/yeosu-aqua-planet.0f4e7057.png"
+  },
+  {
+    "revision": "4104a58ab82a2c464301e33417761747",
+    "url": "/aquarium/static/media/yeosu_gate_video.4104a58a.mp4"
+  }
+]);
